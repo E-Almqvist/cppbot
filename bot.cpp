abdafe366e13681dc5503057a521d43000a0f63f
E. Almqvist <h1g.server@gmail.com>
2020-02-11 19:56:54 +0100

    Stuff

--- bot.cpp
+++ bot.cpp
@@ -1,7 +1,31 @@
-#define PREFIX_ "[DEBUG]"
+#define PRINT_PREFIX "[DEBUG] "
+#define BOTID "675743438117011506"
 #include "sleepy_discord/websocketpp_websocket.h"
 
+void print( std::string txt ) {
+    std::cout << PRINT_PREFIX << txt << std::endl;
+}
+
+class BotClient : public SleepyDiscord::DiscordClient {
+    public:
+        using SleepyDiscord::DiscordClient::DiscordClient;
+
+        void onReady() {
+            updateStatus( "Test Bot" );
+        }
+
+        void onMessage( SleepyDiscord::Message msg ) {
+            print( msg.author.username + "#" + msg.author.discriminator + " sent: " + msg.content );
+
+            if( msg.isMentioned( BOTID ) ) {
+                sendMessage( msg.channelID, "Hello" );
+            }
+        }
+};
+
 int main() {
-    std::cout << PREFIX_ << "test" << std::endl;
+    BotClient client( "TOKEN", 2 );
+    client.run();
+    
     return 0;
 }
